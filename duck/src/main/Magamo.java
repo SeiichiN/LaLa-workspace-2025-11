951d0b91dda77fc45013201fddaabf42ece8f5ac
SeiichiN <billie175@gmail.com>
2025-11-13 09:33:44 +0900

    Magamo.java modified.

--- duck/src/main/Magamo.java
+++ duck/src/main/Magamo.java
@@ -1,6 +1,6 @@
 package main;
 
-public class Magamo extends FlySwimBird {
+public class Magamo extends Bird implements Flying, Swimming {
 	public Magamo(String name) {
 		super(name);
 	}
@@ -8,6 +8,18 @@ public class Magamo extends FlySwimBird {
 	@Override
 	public void say() {
 		System.out.println(this.name + "はガアガアと鳴く。");
+	}
+
+	@Override
+	public void swim() {
+		System.out.println(this.name + "はスイスイ泳ぐ。");
+		
+	}
+
+	@Override
+	public void fly() {
+		System.out.println(this.name + "はバタバタと飛ぶ。");
+		
 	}
 	
 }
