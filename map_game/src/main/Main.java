065b43f3b0c139badb9897daee5592b821dc193f
SeiichiN <billie175@gmail.com>
2025-11-18 17:16:16 +0900

    マップの作成

--- map_game/src/main/Main.java
+++ map_game/src/main/Main.java
@@ -2,6 +2,25 @@ package main;
 
 public class Main {
 
+	public static void setPosition(char ch, char[][] map) {
+		int y, x;
+		do {
+			y = new java.util.Random().nextInt(5);
+			x = new java.util.Random().nextInt(5);
+		} while(map[y][x] != '.');
+		map[y][x] = ch;
+	}
+
+	public static void printMap(char[][] map) {
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 5; x++) {
+				System.out.print("|" + map[y][x]);
+			}
+			System.out.println("|");
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		char[][] map = {
 				{'.', '.', '.', '.', '.'},
@@ -10,31 +29,10 @@ public class Main {
 				{'.', '.', '.', '.', '.'},
 				{'.', '.', '.', '.', '.'}
 		};
-		int y, x;
-		do {
-			y = new java.util.Random().nextInt(5);
-			x = new java.util.Random().nextInt(5);
-		} while(map[y][x] != '.');
-		map[y][x] = 'g';
-		
-		do {
-			y = new java.util.Random().nextInt(5);
-			x = new java.util.Random().nextInt(5);
-		} while(map[y][x] != '.');
-		map[y][x] = 's';
-		
-		do {
-			y = new java.util.Random().nextInt(5);
-			x = new java.util.Random().nextInt(5);
-		} while(map[y][x] != '.');
-		map[y][x] = 'p';
-		
-		for (y = 0; y < 5; y++) {
-			for (x = 0; x < 5; x++) {
-				System.out.print("|" + map[y][x]);
-			}
-			System.out.println("|");
-		}
+		setPosition('g', map);
+		setPosition('s', map);
+		setPosition('p', map);
+		printMap(map);
 		
 	}
 
