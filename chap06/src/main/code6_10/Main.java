d8dbdcaf823c71996604fcc44e6b0a1f22338114
SeiichiN <billie175@gmail.com>
2025-11-19 16:23:57 +0900

    11/19 p252まで

---
+++ chap06/src/main/code6_10/Main.java
@@ -0,0 +1,14 @@
+package main.code6_10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] heights = {172, 140, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		for (int h : heights) {
+			System.out.println(h);
+		}
+
+	}
+
+}
